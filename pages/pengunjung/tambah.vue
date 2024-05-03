042bfdd5a981cfed30e28cb7e7d496b7fe5e9fa5
jahratunnisa <141713515+jahratunnisa@users.noreply.github.com>
2024-05-03 11:05:42 +0700

    menambahkan statistik

--- pages/pengunjung/tambah.vue
+++ pages/pengunjung/tambah.vue
@@ -152,7 +152,7 @@
         <nuxt-link to="/pengunjung">
           <div class="card bg-pengunjung rounded-5 bg-primary">
             <div class="card-body">
-              <h2 class="text-light">pengunjung</h2>
+              <h2 class="text-light">Pengunjung</h2>
             </div>
           </div>
         </nuxt-link>
@@ -167,7 +167,28 @@
           </div>
         </nuxt-link>
       </div>
+    </div>
+    <h2>STATISTIK</h2>
+    <div class="row py-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung">
+          <div class="card pengunjung rounded-5 st">
+            <div class="card-body">
+              <h1 class="text-center" style="margin-top: 80px;"> 1 PENGUNJUNG</h1>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
 
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card buku rounded-5 st">
+            <div class="card-body">
+              <h1 class="text-center" style="margin-top: 80px;">4 BUKU</h1>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -176,6 +197,7 @@
 .card {
   height: 250px;
   box-shadow: 1px 1px 10px #424242;
+  
 }
 .card.bg-pengunjung {
   background-image: url("@/assets/img/kunjungan.jpeg");
@@ -186,5 +208,9 @@
 .card.bg-buku {
   background: url("@/assets/img/caribuku.jpg") no-repeat center center;
   background-size: cover;
+}
+
+.st{
+  background-color: #7ac4dd92;
 }
 </style>
